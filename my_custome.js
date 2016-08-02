12c39620eaa8c0e0d53334fdf41558cfc1a899ac
Phuong.NLT <Phuong.NLT>
2016-08-02 14:39:07 +0700

    first

---
+++ my_custome.js
@@ -0,0 +1,19 @@
+(function(){
+	'use strict';
+
+	var appjs = {
+		init: function(){
+			appjs.Event_init();
+			var asr = 'asdasd';
+		},
+		get_myprofile: function(){
+			var wwww = 'qweqwe';
+		},
+		event_init: function(){
+			var aa = 'qweqweqwe';
+		}
+		
+	};
+
+	appjs.init();
+})()
---
+++ regex.php
@@ -0,0 +1,119 @@
+<?php
+// $html = '<h1>見出し1：ページタイトルpandagau</h1> <h2>見出し2</h2> <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p> <h3>見出し3</h3> <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p> <ul> <li>リストリストリストリストリスト </li> <li>リストリストリストリストリスト </li> <li>リストリストリストリストリスト </li> </ul> <h4>見出し4</h4> <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p> <h5>見出し5</h5> <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p> <h6>見出し6</h6> <p><img class="float_lft" alt="#" width="200" height="150" src="#1#" /></p> <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテ
+// キストテキストテキスト</p> <p class="clear"> </p> <p><img class="float_rgt" alt="#" width="200" height="150" src="#1#" /></p> <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p> <table class="datatable" border="1" width="100%"> <tbody> <tr> <th>見出しセル</th> <td>項目セル</td> <td>項目セル</td> </tr> <tr> <th>見出しセル</th> <td>項目セル</td> <td>項目セル</td> </tr> <tr> <th>見出しセル</th> <td>項目セル</td> <td>項目セル</td> </tr> </tbody> </table> <table class="outline"> <tbody> <tr> <!--#1#--><!--#2#--> <td> <p>本文が入ります</p> </td> <!--#3#--><!--#4#--> </tr> </tbody> </table> <hr /> <table class="col2"> <tbody> <tr> <!--#5#--><!--#6#--> <td class="col2L"> <table class="box_menu"> <tbody> <tr> <!--#7#--><!--#8#--> <td class="box_menu_ttl"> <p>メニュータイトル</p> </td> <!--#9#--><!--#10#--> </tr> <tr> <!--#11#--><!--#12#--> <td class="box_menu_cnt"> <ul> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> </ul> </td> <!--#13#--><!--#14#--> </tr> </tbody> </table> </td> <!--#15#--><!--#16#--> <td class="col2R"> <table class="box_menu"> <tbody> <tr> <!--#7#--><!--#8#--> <td class="box_menu_ttl"> <p>メニュータイトル</p> </td> <!--#9#--><!--#10#--> </tr> <tr> <!--#11#--><!--#12#--> <td class="box_menu_cnt"> <ul> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> </ul> </td> <!--#13#--><!--#14#--> </tr> </tbody> </table> </td> <!--#17#--><!--#18#--> </tr> </tbody> </table> <hr /> <table class="col2"> <tbody> <tr> <!--#5#--><!--#6#--> <td class="col2L"> <ul class="box_btn"> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> </ul> </td> <!--#15#--><!--#16#--> <td class="col2R"> <ul class="box_btn"> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> </ul> </td> <!--#17#--><!--#18#--> </tr> </tbody> </table> <hr /> <table class="box_info"> <tbody> <tr> <!--#19#--><!--#20#--> <td class="box_info_ttl"> <p>お知らせ</p> </td> <!--#21#--><!--#22#--> </tr> <tr> <!--#23#--><!--#24#--> <td class="box_info_cnt"> <ul> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> </ul> </td> <!--#25#--><!--#26#--> </tr> </tbody> </table> <hr /> <table class="box_link"> <tbody> <tr> <!--#27#--><!--#28#--> <td class="box_link_ttl"> <p>関連リンク</p> </td> <!--#29#--><!--#30#--> </tr> <tr> <!--#31#--><!--#32#--> <td class="box_link_cnt"> <ul> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> <li><a href="#2#">テキストテキストテキスト</a> </li> </ul> </td> <!--#33#--><!--#34#--> </tr> </tbody> </table>';
+// //$html = 'qqqqqq<q> </q>eeeee';
+// $pattern = '/( | )/';
+// $pattern = '/(<([\w]+)[^>]*>)( | )(( | )+)?(<\/([^>]*)>)/i';
+// //$pattern = '/(<([\w]+)[^>]*>)( | )?(<\/([^>]*)>)/i';
+// $subject = 'demo';
+// $r = '';
+// if (preg_match($pattern, $html, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'Chuỗi regex so khớp';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	//echo htmlentities($html_entities);
+// }
+
+// $str_cut = substr($html, 1887);
+// //echo htmlentities($str_cut);
+
+// //get "None capturing group" in $subject
+// $subject = 'program (None capturing group "Welcome To You") )))))';
+// $pattern = '/\((.+)"(?:.+)"\)/';
+// //$pattern = '/\((.+)"(.+)"\)/';
+// if (preg_match($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'None Capturing Group, use ?:';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	echo htmlentities($html_entities);
+// }
+
+// //get domain email in $subject
+// $subject = 'thehalfheart@gmail...com, freetuts@facebook.com, thehalfheart@yahoo.com';
+// $pattern = '/@(\w+?)([\.])(\w+[^,]+)/';
+
+// if (preg_match($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'Lấy đuôi email trong chuỗi';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	echo htmlentities($html_entities);
+// }
+
+// $subject = 'thehalfheart@gmail...com, freetuts@facebook.com, thehalfheart@yahoo.com';
+// $pattern = '/@\w+?[\.]\w+[^,]+/';
+// if (preg_match_all($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'Lấy đuôi email trong chuỗi';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	echo htmlentities($html_entities);
+// }
+
+// // get domain email of thehalfheart
+// $subject = 'thehalfheart@gmail.com, freetuts@facebook.com, thehalfheart@yahoo.com';
+// $pattern = '/(?<=thehalfheart)@\w+?[\.]\w+[^,]+/';
+// if (preg_match_all($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'Position lookbehind, use ?<=';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	echo htmlentities($html_entities);
+// }
+
+
+// // get domain email not of thehalfheart
+// $subject = ' thehalfheart@gmail.com, freetuts@facebook.com, thehalfheart@yahoo.com, room@freetuts.net';
+// $pattern = '/(?<!thehalfheart)@\w+?[\.]\w+[^,]+/';
+// if (preg_match_all($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'Negative lookbehind, use ';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	echo htmlentities($html_entities);
+// }
+
+// // get name of domain gmail.com
+// $subject = 'freetuts@gmail.com thehalfheart@facebook.com hoclaptrinh@yahoo.com freetuts.net@gmail.com';
+// $pattern = '/[^\s]+(?=@gmail\.com)/';
+// if (preg_match_all($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'Position lookahead, use ?=';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	echo htmlentities($html_entities);
+// }
+
+// // get name of domain gmail.com
+// $subject = 'freetuts@gmail.com thehalfheart@facebook.com hoclaptrinh@yahoo.com freetuts.net@gmail.com';
+// $pattern = '/[^\s]+(?!@gmail\.com)/';
+//  // $subject   = '<h1>{prim_practice_name}</h1><p>{prim_content}</p><p>Our Locations Are {location_loop}{name} - {state}<br/>{/location_loop}</p>';
+//  // $pattern = '/\{((?!\w*_loop\})\w*)\}/'; '/\{(\w*(?!\_loop))\}/'
+// if (preg_match_all($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+//     echo 'Negative lookahead, use ?! sai';
+// 	echo '<pre>';$html_entities = print_r($r, true);
+// 	echo htmlentities($html_entities);
+// }
+
+// get name of domain gmail.com
+$subject = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="abc" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>ページタイトル</title>
+<!-- InstanceEndEditable -->
+<meta name="author" content="黒出市" />
+<meta name="viewport" content="width=device-width, maximum-scale=3.0" />
+<meta name="format-detection" content="telephone=no" />
+<link href="/cms8341/shared/style/default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/cms8341/shared/style/shared.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/cms8341/shared/style/color/color0.css" rel="stylesheet" type="text/css" media="all" id="tmp_color" class="tmp_color" title="default" />
+<link href="/cms8341/shared/style/color/color1.css" rel="alternate stylesheet" type="text/css" media="all" class="tmp_color" title="darkblue" />
+<link href="/cms8341/shared/style/color/color2.css" rel="alternate stylesheet" type="text/css" media="all" class="tmp_color" title="yellow" />
+<link href="/cms8341/shared/style/color/color3.css" rel="alternate stylesheet" type="text/css" media="all" class="tmp_color" title="black" />
+<link href="/cms8341/shared/templates/base/style/edit.css" rel="stylesheet" type="text/css" media="all" />
+<link href="/cms8341/shared/style/smartphone.css" rel="stylesheet" media="only screen and (max-width : 480px)" type="text/css" id="tmp_smartphone_css" />
+<link href="/cms8341/shared/images/favicon/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+<link href="/cms8341/shared/images/favicon/apple-touch-icon-precomposed.png" rel="apple-touch-icon-precomposed" />
+<script type="text/javascript" src="/cms8341/shared/js/jquery.js"></script>
+<script type="text/javascript" src="/cms8341/shared/js/gd.js"></script>
+<script type="text/javascript" src="/cms8341/shared/js/setting_head.js"></script>
+<!-- InstanceBeginEditable name="custom" -->
+<!-- InstanceEndEditable -->
+<!-- InstanceParam name="inquiry_flg" type="boolean" value="false" -->
+<!-- InstanceParam name="plugin_flg" type="boolean" value="true" -->';
+$pattern = '/<\s*meta[^>]*name\s*=\s*"keywords"[^>]*>/is';
+if (preg_match_all($pattern, $subject, $r, PREG_OFFSET_CAPTURE, 0)){
+    echo 'Position lookahead, use ?=';
+	echo '<pre>';$html_entities = print_r($r, true);
+	echo htmlentities($html_entities);
+}
